c4ba2365464ee963f91f4e877610dd71e161b6c8
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-09 19:07:56 +0900

    Add HTTP Server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/unsafe-risk/ulengthen
 
 go 1.19
+
+require github.com/lemon-mint/envaddr v0.0.0-20220724120637-f2bfd2710deb
--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/lemon-mint/envaddr"
+)
+
+func main() {
+	srv := http.Server{}
+	ln, err := net.Listen("tcp", envaddr.Get(":8080"))
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+	fmt.Printf("Listening on %s\n", ln.Addr())
+	go srv.Serve(ln)
+
+	{
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+		<-sig
+		fmt.Println("\nShutting down...")
+		err = srv.Shutdown(nil)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+		fmt.Println("Done.")
+	}
 }
